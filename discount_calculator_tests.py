020452555b7f8c93d198a6c2301f022dd6c2703a
ajaanbaahu <mohanrathish@gmail.com>
2014-06-02 22:45:32 -0700

    new change

--- discount_calculator_tests.py
+++ discount_calculator_tests.py
@@ -2,18 +2,24 @@ import unittest
 from discount_calculator import DiscountCalculator
 
 class DiscountCalculatorTests(unittest.TestCase):
-  def test_ten_percent_discount(self):
-    discount_calculator = DiscountCalculator()
-    result = discount_calculator.calculate(100,10,'percent')
-    self.assertEqual(10.0, result)
+    def test_ten_percent_discount(self):
+        discount_calculator = DiscountCalculator()
+        result = discount_calculator.calculate(100,10,'percent')
+        self.assertEqual(10.0, result)
 
-  def fifteen_percent_discount_test(self):
-    discount_calculator = DiscountCalculator()
-    result = discount_calculator.calculate(100,15,'percent')
-    self.assertEqual(15.0, result)
+    def fifteen_percent_discount_test(self):
+        discount_calculator = DiscountCalculator()
+        result = discount_calculator.calculate(100,15,'percent')
+        self.assertEqual(15.0, result)
 
-  def five_off_discount_test(self):
+    def five_off_discount_test(self):
 
-      discount_calculator=DiscountCalculator()
-      result=discount_calculator.calculate(250,5,'absolute')
-      self.assertEqual(5,result)
+        discount_calculator=DiscountCalculator()
+        result=discount_calculator.calculate(250,5,'absolute')
+        self.assertEqual(5,result)
+
+    def type_error_test(self):
+
+        discount_calc=DiscountCalculator()
+
+        self.assertRaises(ValueError,discount_calc.calculate,250,5,'random')
