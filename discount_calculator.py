d9dd8d06d4c96b20f51f767ed22c27056210acf3
ajaanbaahu <mohanrathish@gmail.com>
2014-06-03 21:13:18 -0700

    new commit

--- discount_calculator.py
+++ discount_calculator.py
@@ -3,10 +3,15 @@ class DiscountCalculator(object):
 
   def calculate(self, total, discount_amount, discount_type):
     if discount_type=='percent':
+        if discount_amount >= 100:
+            raise ValueError("Percent discount cannot be more than 100%")
+
         percentage_discount = float(discount_amount) / 100
         discount = float(total) * percentage_discount
 
     elif discount_type=='absolute':
+        if discount_amount>=total:
+            raise ValueError("Absolute discount cannot be more than total")
         discount=discount_amount
 
     else:
